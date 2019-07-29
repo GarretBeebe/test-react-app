2b95d8783238a55874d00c0ce4cce3d017b91c4e
Garret Beebe <garret@TK-421-MAC.local>
2019-07-28 21:17:03 -0700

    cleaning up files

--- src/components/board.js
+++ src/components/board.js
@@ -64,4 +64,4 @@ export class Board extends React.Component {
     }
 	}
 
-	export default Board;
+export default Board;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import Game from './components/game';
 
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
